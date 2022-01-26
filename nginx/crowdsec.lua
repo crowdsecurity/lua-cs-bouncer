3a3812ede81248354cffe7645de99660c2f56c78
alteredCoder <kevin@crowdsec.net>
2022-01-26 18:53:23 +0100

    update

--- nginx/crowdsec.lua
+++ nginx/crowdsec.lua
@@ -351,6 +351,7 @@ function csmod.Allow(ip)
       if remediation == "ban" then
           ret_code = runtime.conf["RET_CODE"]
           ngx.req.set_header("Location", runtime.conf["REDIRECT_PATH"])
+          ngx.req.uri = runtime.conf["REDIRECT_PATH"]
           ngx.status = utils.HTTP_CODE[ret_code]
           ngx.exit()
           return
