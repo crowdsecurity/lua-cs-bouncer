6c4cfde557a4f2f88ce98a1aa6254b8b06dcd968
alteredCoder <kevin@crowdsec.net>
2022-01-30 16:24:34 +0100

    add the template lib

---
+++ nginx/template.lua
@@ -0,0 +1,24 @@
+local template = {}
+
+function template.escape(data)
+  return tostring(data or ''):gsub("[\">/<'&]", {
+    ["&"] = "&amp;",
+    ["<"] = "&lt;",
+    [">"] = "&gt;",
+    ['"'] = "&quot;",
+    ["'"] = "&#39;",
+    ["/"] = "&#47;"
+  })
+end
+
+function template.new(template_str, args)
+
+    for k, v in pairs(args) do
+        local var = "{{" .. k .. "}}"
+        template_str = template_str:gsub(var, v)
+    end
+
+    return template_str
+end
+
+return template
