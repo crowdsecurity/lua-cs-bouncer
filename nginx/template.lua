cec44d254997f67f613a758bdef2abf8eefa1667
alteredCoder <kevin@crowdsec.net>
2022-01-30 16:47:42 +0100

    update

--- nginx/template.lua
+++ nginx/template.lua
@@ -1,16 +1,5 @@
 local template = {}
 
-function template.escape(data)
-  return tostring(data or ''):gsub("[\">/<'&]", {
-    ["&"] = "&amp;",
-    ["<"] = "&lt;",
-    [">"] = "&gt;",
-    ['"'] = "&quot;",
-    ["'"] = "&#39;",
-    ["/"] = "&#47;"
-  })
-end
-
 function template.compile(template_str, args)
 
     for k, v in pairs(args) do
@@ -18,7 +7,7 @@ function template.compile(template_str, args)
         template_str = template_str:gsub(var, v)
     end
 
-    return template.escape(template_str)
+    return template_str
 end
 
 return template
