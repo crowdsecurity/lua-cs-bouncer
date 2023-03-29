902f0550235828898cb9540bb9e0d48acd7121de
Laurence Jones <laurence.jones@live.co.uk>
2023-03-29 10:07:30 +0100

    Add other captcha providers / update templates (#39)
    * Add option for another captcha providers (hcaptcha, turnstile)
    
    * Fix wrong map usage
    
    * Update templates to mobile first and light/dark mode options
    
    * rename recaptcha to captcha more generic
    
    * Just one more update to show pointer cursor on submit button
    
    * height on mobile was out fixed
    
    * Edit title
    
    * Make button same color dark and light mode easily to see
    
    * Add auto captcha submit on completion and increase height on ban template on mobile
    
    * Fix typo
    
    * Fix dark mode button on ban
    
    * Add short timeout to make captcha smoother
    
    * half timeout to make captcha smoother
    
    * Fix the things I noticed when updating the haproxy bouncer
    
    * Fix light mode on load within templates

--- lib/plugins/crowdsec/captcha.lua
+++ lib/plugins/crowdsec/captcha.lua
@@ -6,7 +6,20 @@ local utils = require "plugins.crowdsec.utils"
 
 local M = {_TYPE='module', _NAME='recaptcha.funcs', _VERSION='1.0-0'}
 
-local recaptcha_verify_url = "https://www.google.com/recaptcha/api/siteverify"
+local captcha_backend_url = {}
+captcha_backend_url["recaptcha"] = "https://www.recaptcha.net/recaptcha/api/siteverify"
+captcha_backend_url["hcaptcha"] = "https://hcaptcha.com/siteverify"
+captcha_backend_url["turnstile"] = "https://challenges.cloudflare.com/turnstile/v0/siteverify"
+
+local captcha_frontend_js = {}
+captcha_frontend_js["recaptcha"] = "https://www.recaptcha.net/recaptcha/api.js"
+captcha_frontend_js["hcaptcha"] = "https://js.hcaptcha.com/1/api.js"
+captcha_frontend_js["turnstile"] = "https://challenges.cloudflare.com/turnstile/v0/api.js"
+
+local captcha_frontend_key = {}
+captcha_frontend_key["recaptcha"] = "g-recaptcha"
+captcha_frontend_key["hcaptcha"] = "h-captcha"
+captcha_frontend_key["turnstile"] = "cf-turnstile"
 
 M._VERIFY_STATE = "to_verify"
 M._VALIDATED_STATE = "validated"
@@ -32,7 +45,7 @@ end
 
 
 
-function M.New(siteKey, secretKey, TemplateFilePath)
+function M.New(siteKey, secretKey, TemplateFilePath, captcha_provider)
 
     if siteKey == nil or siteKey == "" then
       return "no recaptcha site key provided, can't use recaptcha"
@@ -57,8 +70,12 @@ function M.New(siteKey, secretKey, TemplateFilePath)
         return "Template file " .. TemplateFilePath .. "not found."
     end
 
+    M.CaptchaProvider = captcha_provider
+
     local template_data = {}
-    template_data["recaptcha_site_key"] =  M.SiteKey
+    template_data["captcha_site_key"] =  M.SiteKey
+    template_data["captcha_frontend_js"] = captcha_frontend_js[M.CaptchaProvider]
+    template_data["captcha_frontend_key"] = captcha_frontend_key[M.CaptchaProvider]
     local view = template.compile(captcha_template, template_data)
     M.Template = view
 
@@ -70,6 +87,9 @@ function M.GetTemplate()
     return M.Template
 end
 
+function M.GetCaptchaBackendKey()
+    return captcha_frontend_key[M.CaptchaProvider] .. "-response"
+end
 
 function table_to_encoded_url(args)
     local params = {}
@@ -77,17 +97,17 @@ function table_to_encoded_url(args)
     return table.concat(params, "&")
 end
 
-function M.Validate(g_captcha_res, remote_ip)
+function M.Validate(captcha_res, remote_ip)
     local body = {
         secret   = M.SecretKey,
-        response = g_captcha_res,
+        response = captcha_res,
         remoteip = remote_ip
     }
 
     local data = table_to_encoded_url(body)
     local httpc = http.new()
     httpc:set_timeout(2000)
-    local res, err = httpc:request_uri(recaptcha_verify_url, {
+    local res, err = httpc:request_uri(captcha_backend_url[M.CaptchaProvider], {
       method = "POST",
       body = data,
       headers = {
