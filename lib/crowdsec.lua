2c5a2a8a94a7b51c6ca7b5acfbf8da1b987a09e8
sabban <github@sabban.eu>
2024-05-10 22:30:11 +0200

    fix int deivions

--- lib/crowdsec.lua
+++ lib/crowdsec.lua
@@ -564,7 +564,7 @@ function csmod.allowIp(ip)
     end
     if in_cache ~= nil then -- we have it in cache
       ngx.log(ngx.DEBUG, "'" .. key .. "' is in cache")
-      metrics:increment(runtime.origins[tostring(flag_id/4)],1)
+      metrics:increment(runtime.origins[tostring(flag_id//4)],1)
       return in_cache, runtime.remediations[tostring(flag_id%4)], nil
     end
   end
@@ -583,8 +583,7 @@ function csmod.allowIp(ip)
     if in_cache ~= nil then -- we have it in cache
       ngx.log(ngx.DEBUG, "'" .. key .. "' is in cache")
       ngx.log(ngx.INFO, "'" .. key .. "' is in cache")
-      ngx.log(ngx.INFO, "flag_id is " .. tostring(flag_id) .. " " .. tostring(flag_id/4) .. " " .. tostring(flag_id%4)) --debug
-      metrics:increment(runtime.origins[tostring(flag_id/4)],1)
+      metrics:increment(runtime.origins[tostring(flag_id//4)],1)
       return in_cache, runtime.remediations[tostring(flag_id%4)], nil
     end
   end
