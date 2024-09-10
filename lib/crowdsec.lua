ce7f960a8a2009a917b159e9ca4ed69791641df3
sabban <github@sabban.eu>
2024-09-10 22:06:26 +0200

    hoip

--- lib/crowdsec.lua
+++ lib/crowdsec.lua
@@ -156,6 +156,7 @@ function csmod.init(configFile, userAgent)
     live:new(
       runtime.conf["API_URL"],
       runtime.conf["CACHE_EXPIRATION"],
+      userAgent,
       runtime.conf["BOUNCING_ON_TYPE"],
       runtime.conf["REQUEST_TIMEOUT"],
       REMEDIATION_API_KEY_HEADER
