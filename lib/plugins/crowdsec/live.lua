7990cba6a04e6973fdc404c32cdc5a4498ba0ec5
sabban <github@sabban.eu>
2024-09-10 23:06:25 +0200

    try this

--- lib/plugins/crowdsec/live.lua
+++ lib/plugins/crowdsec/live.lua
@@ -15,110 +15,33 @@ live.cache = ngx.shared.crowdsec_cache
 -- @param api_key_header string: the header to use for the API key
 -- @return live: the live object
 
-function live:new(api_url, cache_expiration, user_agent, bouncing_on_type, time_out, api_key_header)
-  local succ, err, forcible = self.cache:set("api_url", api_url)
+function live:new(api_url)
+  local succ, err, forcible = live.cache:set("api_url", api_url)
   if not succ then
-    ngx.log(ngx.ERR, "failed to add key api_url in cache: "..err)
+    ngx.log(ngx.ERR, "failed to add key about captcha for ip '" .. ip .. "' in cache: "..err)
   end
   if forcible then
     ngx.log(ngx.ERR, "Lua shared dict (crowdsec cache) is full, please increase dict size in config")
   end
-
-  local succ, err, forcible = self.cache:set("cache_expiration", cache_expiration)
-  if not succ then
-    ngx.log(ngx.ERR, "failed to add key cache_expiration in cache: "..err)
-  end
-  if forcible then
-    ngx.log(ngx.ERR, "Lua shared dict (crowdsec cache) is full, please increase dict size in config")
-  end
-
-  ngx.log(ngx.INFO, "debug" .. user_agent)
-  local succ, err, forcible = self.cache:set("user_agent", user_agent)
-  if not succ then
-    ngx.log(ngx.ERR, "failed to add key user_agent in cache: ".. err)
-  end
-  if forcible then
-    ngx.log(ngx.ERR, "Lua shared dict (crowdsec cache) is full, please increase dict size in config")
-  end
-
-  local succ, err, forcible = self.cache:set("bouncing_on_type", bouncing_on_type)
-  if not succ then
-    ngx.log(ngx.ERR, "failed to add key bouncing_on_type in cache: "..err)
-  end
-  if forcible then
-    ngx.log(ngx.ERR, "Lua shared dict (crowdsec cache) is full, please increase dict size in config")
-  end
-
-  local succ, err, forcible = self.cache:set("time_out", time_out)
-  if not succ then
-    ngx.log(ngx.ERR, "failed to add key time_out in cache: "..err)
-  end
-  if forcible then
-    ngx.log(ngx.ERR, "Lua shared dict (crowdsec cache) is full, please increase dict size in config")
-  end
-
-  ngx.log(ngx.INFO, "debug" .. api_key_header)
-  local succ, err, forcible = self.cache:set("api_key_header", api_key_header)
-  if not succ then
-    ngx.log(ngx.ERR, "failed to add key api_key_header in cache: "..err)
-  end
-  if forcible then
-    ngx.log(ngx.ERR, "Lua shared dict (crowdsec cache) is full, please increase dict size in config")
-  end
-
   return self
 end
 
 --- Live query the API to get the decision for the IP
 -- Query the live API to get the decision for the IP in real time
 -- @param ip string: the IP to query
+-- @param timeout number: the timeout of the request to lapi
+-- @param cache_expiration number: the expiration time of the cache
+-- @param api_key_header string: the authorization header to use for the lapi request
+-- @param api_key string: the API key to use for the lapi request
+-- @param user_agent string: the user agent to use for the lapi request
+-- @param ssl_verify boolean: whether to verify the SSL certificate or not
+-- @param bouncing_on_type string: the type of decision to bounce on
 -- @return boolean: true if the IP is allowed, false if the IP is blocked
 -- @return string: the type of the decision
 -- @return string: the origin of the decision
 -- @return string: the error message if any
 
-function live:live_query(ip, api_key)
-  -- debug: wip
-  ngx.log(ngx.INFO, "live_query: " .. ip)
-  local cache_expiration = self.cache:get("cache_expiration")
-  if not cache_expiration then
-    ngx.log(ngx.ERR, "cache_expiration not found in cache")
-    return true, nil, nil, nil
-  end
-  local bouncing_on_type = self.cache:get("bouncing_on_type")
-  if not cache_expiration then
-    ngx.log(ngx.ERR, "bouncing_on_type not found in cache")
-    return true, nil, nil, nil
-  end
-  local api_url = self.cache:get("api_url")
-  if not api_url then
-    ngx.log(ngx.ERR, "api_url not found in cache")
-    return true, nil, nil, nil
-  end
-
-  local timeout = self.cache:get("time_out")
-  if not api_url then
-    ngx.log(ngx.ERR, "time_out not found in cache")
-    return true, nil, nil, nil
-  end
-
-  local api_key_header = self.cache:get("api_key_header")
-  if not api_key_header then
-    ngx.log(ngx.ERR, "api_key_header not found in cache")
-    return true, nil, nil, nil
-  end
-
-  local user_agent = self.cache:get("user_agent")
-  if not user_agent then
-    ngx.log(ngx.ERR, "user_agent not found in cache")
-    return true, nil, nil, nil
-  end
-
-  local ssl_verify = self.cache:get("ssl_verify")
-  if not ssl_verify then
-    ngx.log(ngx.ERR, "user_agent not found in cache")
-    return true, nil, nil, nil
-  end
+function live:live_query(ip, timeout, cache_expiration, api_key_header, api_key, user_agent, ssl_verify, bouncing_on_type)
 
   local link = api_url .. "/v1/decisions?ip=" .. ip
   -- function M.get_remediation_http_request(link,timeout, api_key_header, api_key, user_agent,ssl_verify)
