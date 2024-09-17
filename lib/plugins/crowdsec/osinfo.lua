d78d192ce86e4eb7a7b3859719809ce9e168962a
sabban <github@sabban.eu>
2024-09-17 20:35:39 +0200

    hop

--- lib/plugins/crowdsec/osinfo.lua
+++ lib/plugins/crowdsec/osinfo.lua
@@ -46,6 +46,21 @@ function osinfo.get_os_info()
 	if not os_info["VERSION_ID"] then
 		os_info["VERSION_ID"] = "Unknown"
 	end
+
+	-- remove quotes
+	-- some distros have quotes around the values
+	-- e.g. "Ubuntu"
+	-- this is not the case for all distros
+	-- so we need to check if the first character is a quote
+	-- and remove it if it is
+ 	for k, _ in pairs(os_info) do
+		if string.sub(os_info[k], 1, 1) == '"' then
+			os_info[k] = string.sub(os_info[k], 2, -1)
+		end
+		if string.sub(os_info[k], -2, -1) == '"' then
+			os_info[k] = string.sub(os_info[k], 1, -2)
+		end
+	end
 	return os_info
 end
 
