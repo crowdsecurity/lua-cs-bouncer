12ef195ad86bef6b828f3a135bf2b60f74b8916e
sabban <github@sabban.eu>
2024-09-17 22:06:11 +0200

    debug

--- lib/plugins/crowdsec/osinfo.lua
+++ lib/plugins/crowdsec/osinfo.lua
@@ -57,8 +57,7 @@ function osinfo.get_os_info()
 		if string.sub(os_info[k], 1, 1) == '"' then
 			os_info[k] = string.sub(os_info[k], 2, -1)
 		end
-		ngx.log(ngx.ERR, "removing last quote: " .. string.sub(os_info[k], -2, -1))
-		if string.sub(os_info[k], -2, -1) == '"' then
+		if string.sub(os_info[k], -1, -2) == '"' then
 			os_info[k] = string.sub(os_info[k], 1, -2)
 		end
 	end
