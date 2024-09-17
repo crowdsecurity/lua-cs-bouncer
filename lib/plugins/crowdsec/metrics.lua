21211dea9fcf7b95022348bf82b5a1896a96f02c
sabban <github@sabban.eu>
2024-09-17 22:31:53 +0200

    try this

--- lib/plugins/crowdsec/metrics.lua
+++ lib/plugins/crowdsec/metrics.lua
@@ -271,7 +271,7 @@ function metrics:toJson(window)
   table.insert(remediation_components,
                metrics_data)
 
-  return cjson.encode({remediation_components=remediation_components, log_processors = nil})
+  return cjson.encode({log_processors = nil, remediation_components=remediation_components})
 end
 
 function metrics:sendMetrics(link, headers, ssl, window)
