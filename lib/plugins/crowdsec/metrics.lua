c90f44da72322078b5aa2169c8812d743bc1054b
sabban <github@sabban.eu>
2024-10-28 18:03:58 +0100

    debug

--- lib/plugins/crowdsec/metrics.lua
+++ lib/plugins/crowdsec/metrics.lua
@@ -206,6 +206,7 @@ local function get_labels_from_key(key)
     return "", {}
   else
     if table[2] ~= nil then
+      ngx.log(ngx.INFO, "labels: " .. table[2])
       labels = utils.string_to_table(table[2])
     end
   end
