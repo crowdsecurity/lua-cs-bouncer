70dcdf46559d5db0a6b46564597621cdca4358d6
sabban <github@sabban.eu>
2024-10-30 14:40:41 +0100

    labels for stream

--- lib/plugins/crowdsec/stream.lua
+++ lib/plugins/crowdsec/stream.lua
@@ -219,8 +219,9 @@ function stream:stream_query(api_url, timeout, api_key_header, api_key, user_age
     local table_count = get_decisions_count()
     for ip_type, table_origin in pairs(table_count) do
       for origin, count in pairs(table_origin) do
-        metrics:add_to_metrics("active_decisions/" .. origin .. "/" .. ip_type)
-        local succ, err, forcible = stream.cache:set("metrics_active_decisions/" .. origin .. "/" .. ip_type, count)
+        local labels = {origin = origin, ip_type = ip_type}
+        metrics:add_to_metrics("active_decisions/" .. utils.table_to_string(labels))
+        local succ, err, forcible = stream.cache:set("metrics_active_decisions/" .. utils.table_to_string(labels), count)
         if not succ then
           ngx.log(ngx.ERR, "failed to add "..  "metrics_active_decisions_" .. origin .. "/" .. ip_type .. ": ".. err)
         end
