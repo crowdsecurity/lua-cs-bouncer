14452ed79e6e3a9279ebfc39687f46dbb7e45cbd
Sebastien Blot <sebastien@crowdsec.net>
2025-06-18 20:26:10 +0200

    only call appsec if it was enabled in the configuration

--- lib/crowdsec.lua
+++ lib/crowdsec.lua
@@ -617,7 +617,7 @@ function csmod.Allow(ip)
   -- check with appSec if the remediation component doesn't have decisions for the IP
   -- OR
   -- that user configured the remediation component to always check on the appSec (even if there is a decision for the IP)
-  if ok == true or (is_always_send_to_appsec() and is_appsec_enabled()) then
+  if is_appsec_enabled() and (ok == true or is_always_send_to_appsec()) then
     local appsecOk, appsecRemediation, status_code, err = csmod.AppSecCheck(ip)
     if err ~= nil then
       ngx.log(ngx.ERR, "AppSec check: " .. err)
